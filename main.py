a3ea174051eea6030c35782a60d781d15f7bcd9d
rmansurbek2197 <rmansurbek2197@gmail.com>
2026-04-18 22:36:25 +0500

    Initial commit

--- main.py
+++ main.py
@@ -1,23 +1,5 @@
-import asyncio
-import aiohttp
+def sum_of_numbers(lst):
+    return sum(lst)
 
-async def fetch_page(session, url):
-    async with session.get(url) as response:
-        return await response.text()
-
-async def fetch_all(session, urls):
-    tasks = []
-    for url in urls:
-        task = asyncio.create_task(fetch_page(session, url))
-        tasks.append(task)
-    results = await asyncio.gather(*tasks)
-    return results
-
-async def main():
-    urls = ["http://example.com/page1", "http://example.com/page2", "http://example.com/page3"]
-    async with aiohttp.ClientSession() as session:
-        htmls = await fetch_all(session, urls)
-        for html in htmls:
-            print(html)
-
-asyncio.run(main())
+sonlar = [1, 2, 3, 4, 5]
+print(sum_of_numbers(sonlar))
